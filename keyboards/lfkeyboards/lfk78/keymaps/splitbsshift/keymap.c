b535e166b56e679035076a001f9e401530b849e7
manzel <manzel@users.noreply.github.com>
2018-01-02 14:01:36 +0100

    Small update to LFK78 split keymap

--- keyboards/lfkeyboards/lfk78/keymaps/splitbsshift/keymap.c
+++ keyboards/lfkeyboards/lfk78/keymaps/splitbsshift/keymap.c
@@ -30,9 +30,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [VANILLA] = SPLIT_BS_SPLIT_SHIFT_KEYMAP(
   /* Keymap VANILLA: (Base Layer) Default Layer
    * ,---------.  ,------------------------------------------------------------.  ,---------.
-   * | F1 | F2 |  |Esc | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |  \| ` |  | Ins|PgUp|
+   * | F1 | F2 |  |Esc | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |  \| ` |  | Ins|Home|
    * |---------|  |------------------------------------------------------------|  |---------|
-   * | F3 | F4 |  |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    BS|  | Del|PgDn|
+   * | F3 | F4 |  |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    BS|  | Del|End |
    * |---------|  |------------------------------------------------------------|  `---------'
    * | F5 | F6 |  |Fnc/Caps|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
    * |---------|  |------------------------------------------------------------|  ,----.
@@ -41,16 +41,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | F9 | F10|  |Ctrl|Win |Alt |      Space            |Alt |Win |Ctrl |    |Lft| Dn |Rig |
    * `---------'  `------------------------------------------------------'    `-------------'
    */
-  KC_F1, KC_F2,       KC_ESC,KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV, KC_INS, KC_PGUP,
-  KC_F3, KC_F4,       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,       KC_DEL, KC_PGDN,
+  KC_F1, KC_F2,       KC_ESC,KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV, KC_INS, KC_HOME,
+  KC_F3, KC_F4,       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,       KC_DEL, KC_END,
   KC_F5, KC_F6,       LT(FUNC, KC_CAPS),  KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,      KC_ENT,
   KC_F7, KC_F8,       KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(FUNC),  KC_UP,
   KC_F9, KC_F10,      KC_LCTL, KC_LGUI, KC_LALT,              KC_SPC,           KC_RALT, KC_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
   /* Keymap WKL: (Base Layer) Default Layer
    * ,---------.  ,------------------------------------------------------------.  ,---------.
-   * |Vol-|Vol+|  |Esc | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |  \| ` |  | Ins|PgUp|
+   * |Vol-|Vol+|  |Esc | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |  \| ` |  | Ins|Home|
    * |---------|  |------------------------------------------------------------|  |---------|
-   * | F3 | F4 |  |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    BS|  | Del|PgDn|
+   * | F3 | F4 |  |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    BS|  | Del|End |
    * |---------|  |------------------------------------------------------------|  `---------'
    * | F5 | F6 |  |Fnc/Caps|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
    * |---------|  |------------------------------------------------------------|  ,----.
@@ -60,17 +60,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `---------'  `------------------------------------------------------'    `-------------'
    */
 [DEFAULT_WKL] = SPLIT_BS_SPLIT_SHIFT_KEYMAP(
-  KC_VOLD, KC_VOLU,   KC_ESC,KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV, KC_INS, KC_PGUP,
-  KC_F3, KC_F4,       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,       KC_DEL, KC_PGDN,
+  KC_VOLD, KC_VOLU,   KC_ESC,KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV, KC_INS, KC_HOME,
+  KC_F3, KC_F4,       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,       KC_DEL, KC_END,
   KC_F5, KC_F6,       LT(FUNC, KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,      KC_ENT,
   KC_F7, KC_F8,       KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(FUNC),  KC_UP,
   KC_F9, KC_F10,      KC_LCTL, KC_NO, KC_LALT,             KC_SPC,              KC_RALT, KC_NO, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 
   /* Keymap OSX: (Base Layer) Default Layer
    * ,---------.  ,------------------------------------------------------------.  ,---------.
-   * |Vol-|Vol+|  |Esc | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |  \| ` |  | Ins|PgUp|
+   * |Vol-|Vol+|  |Esc | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |  0| - | = |  \| ` |  | Ins|Home|
    * |---------|  |------------------------------------------------------------|  |---------|
-   * | F3 | F4 |  |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    BS|  | Del|PgDn|
+   * | F3 | F4 |  |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    BS|  | Del|End |
    * |---------|  |------------------------------------------------------------|  `---------'
    * | F5 | F6 |  |Fnc/Caps|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
    * |---------|  |------------------------------------------------------------|  ,----.
@@ -80,17 +80,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `---------'  `------------------------------------------------------'    `-------------'
    */
 [DEFAULT_OSX] = SPLIT_BS_SPLIT_SHIFT_KEYMAP(
-  KC_VOLD, KC_VOLU,   KC_ESC,KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV, KC_INS, KC_PGUP,
-  KC_F3, KC_F4,       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,       KC_DEL, KC_PGDN,
+  KC_VOLD, KC_VOLU,   KC_ESC,KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV, KC_INS, KC_HOME,
+  KC_F3, KC_F4,       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,       KC_DEL, KC_END,
   KC_F5, KC_F6,       LT(FUNC, KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,      KC_ENT,
   KC_F7, KC_F8,       KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT, MO(FUNC), KC_UP,
   KC_F9, KC_F10,      KC_LCTL, KC_LALT, KC_LGUI,             KC_SPC,            KC_RGUI, KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 
   /* Keymap FUNCTION: Function Layer
    * ,---------.  ,-------------------------------------------------------------.  ,---------.
-   * | V- | V+ |  |Slp|F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|     | ` |  |PrSc|Home|
+   * | V- | V+ |  |Slp|F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|     | ` |  |PrSc|PgUp|
    * |---------|  |-------------------------------------------------------------|  |---------|
-   * |    |    |  |Tab  |Hom| Up|End|PgU|   |   |   |   |   |Ply|   |   |  Del  |  | Del|End |
+   * |    |    |  |Tab  |Hom| Up|End|PgU|   |   |   |   |   |Ply|   |   |  Del  |  | Del|PgDn|
    * |---------|  |-------------------------------------------------------------|  `---------'
    * |    |    |  |        |Lft|Dn |Rig|PgD|   |Lft|Dwn| Up|Rgt|   |   |        |
    * |---------|  |-------------------------------------------------------------|   ,----.
@@ -100,8 +100,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `---------'  `------------------------------------------------------'     `--------------'
    */
 [FUNC] = SPLIT_BS_SPLIT_SHIFT_KEYMAP(
-  KC_TR,   KC_TR,    KC_SLEP,  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_TR,  KC_PSCR, KC_HOME,
-  KC_TR,   KC_TR,    KC_NO,KC_HOME, KC_UP, KC_END, KC_PGUP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MPLY, KC_NO, KC_NO,        KC_DEL,  KC_TR, KC_END,
+  KC_TR,   KC_TR,    KC_SLEP,  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_TR,  KC_PSCR, KC_PGUP,
+  KC_TR,   KC_TR,    KC_NO,KC_HOME, KC_UP, KC_END, KC_PGUP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MPLY, KC_NO, KC_NO,        KC_DEL,  KC_TR, KC_PGDN,
   KC_TR,   KC_TR,    KC_TR,   KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,    KC_NO,
   KC_TR,   KC_TR,    KC_TR,     KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_NO, KC_NO, KC_MUTE, KC_NO, KC_NO, KC_NO, TG(SETTINGS), KC_TR,         KC_VOLU,
   KC_TR,   KC_TR,    KC_TR,  KC_TR, KC_TR,                  KC_PGDN,                           KC_TR, KC_TR, KC_TR,         KC_MPRV,  KC_VOLD, KC_MNXT),
